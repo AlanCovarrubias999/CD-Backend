2bacbde463ab0c49be3f39d7586f1498db330b02
AlanCovarrubias999 <alsacovarrubiaspe@ittepic.edu.mx>
2026-01-16 15:22:54 -0600

    Creacion del login estableciendo token de acceso...

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,30 +1,29 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import { createAccessToken } from '../libs/jwt.js';
 import dotenv from 'dotenv/config.js';
 
 export const register = async (req, res) => {
-    const {email, password, username} = req.body;
+    const { email, password, username } = req.body;
 
     try {
         const passwordHash = await bcrypt.hash(password, 10);
 
         const newUser = new User({
-            email, 
+            email,
             password: passwordHash,
             username
         });
 
         const savedUser = await newUser.save();
+        const token = await createAccessToken({ id: savedUser._id });
+        res.cookie('token', token);
+        res.json({
+            id: savedUser._id,
+            email: savedUser.email,
+            username: savedUser.username
+        });
 
-        jwt.sign(
-            {id: savedUser._id}, 
-            process.env.JWT_SECRET, 
-            {expiresIn: '1d'},
-            (err, token) => {
-                if (err) console.log(err);
-                res.json({token});
-            });
 
     } catch (error) {
         console.error("Error al registrar el usuario:", error);
@@ -32,7 +31,34 @@ export const register = async (req, res) => {
     }
 
     console.log(email, password, username);
-}
-export const login = (req, res) => {
-    
-}
+};
+
+export const login = async (req, res) => {
+    const { password, username } = req.body;
+
+    try {
+
+        const userFound = await User.findOne({ username });
+
+        if (!userFound) return res.status(400).json({ message: "Usuario no encontrado" });
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+
+        if (!isMatch) return res.status(400).json({ message: "Credenciales incorrectas" });
+
+        const token = await createAccessToken({ id: userFound._id });
+        res.cookie('token', token);
+        res.json({
+            id: userFound._id,
+            email: userFound.email,
+            username: userFound.username
+        });
+
+
+    } catch (error) {
+        console.error("Error al registrar el usuario:", error);
+        res.status(500).send("Error al registrar el usuario");
+    }
+
+    console.log( password, username);
+};
---
+++ src/libs/jwt.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv/config.js';
+import jwt from 'jsonwebtoken';
+
+export function createAccessToken(payload) {
+    return new Promise((resolve, reject) => {
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            {expiresIn: '1d'},
+            (err, token) => {
+                if (err) reject(err);
+                resolve(token);
+            }
+        );
+    });
+}
