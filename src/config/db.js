4a2f6bfac8960553baca07aa07fb4c1461893cdb
Alan Covarrubias <alanscp09@gmail.com>
2026-06-10 12:07:13 -0700

    fix: database connection issues

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
     try {
         const dbURI = process.env.MONGODB_URI;
         await mongoose.connect(dbURI);
-        console.log("Base de datos conectada");
+        console.log("Base de datos conectada exitosamente");
     } catch (error) {
         console.error("Error al conectar a la base de datos:", error);
         process.exit(1);
